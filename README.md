1835ab52f1d26732fd9b671070a1b1df2096800f
Ahmet Emrebas <aemrebasus@gmail.com>
2020-10-28 23:31:23 -0500

    Frontend initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## ORANGE CHECK
+
+Orange Check is a project management application, powered by Angular 10, Angular Material, and Java Spring Boot, Hibernate, Spring Web and Security. 
---
+++ orange-check-web/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EntityDataModule } from '@ngrx/data';
+import { entityConfig } from './entity-metadata';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    EntityDataModule.forRoot(entityConfig),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
