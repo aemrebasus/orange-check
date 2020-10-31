22d52e876ecace951208d44c47ac861fd73f97f1
Ahmet Emrebas <aemrebasus@gmail.com>
2020-10-31 03:26:57 -0500

    configuration-gh-pages

--- orange-check-web/src/app/contants.ts
+++ orange-check-web/src/app/contants.ts
@@ -2,4 +2,4 @@ import { environment } from 'src/environments/environment';
 
 export const APPLICATION_BASE_ROUTE = 'app';
 export const WEBSITE_BASE_ROUTE = 'pages';
-export const API_BASE_ROUTE = environment.githubpage ? 'orange-check-web/assets/api' : 'assets/api';
+export const API_BASE_ROUTE = environment.production ? 'api/v1' : 'assets/api';
