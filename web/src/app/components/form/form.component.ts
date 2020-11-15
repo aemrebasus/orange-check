3276ace3db60594dfb63fd013183a747dd920a50
Ahmet Emrebas <aemrebasus@gmail.com>
2020-11-15 15:51:20 -0600

    Form SUbmission

--- web/src/app/components/form/form.component.ts
+++ web/src/app/components/form/form.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { issueForm, messageForm, projectForm, userForm } from '@components/entity-forms';
+import { EntityCollectionServiceBase } from '@ngrx/data';
+import { IssueDataService, MessageDataService, ProjectDataService, UserDataService } from '@services/entities.data.service';
 import { AeDynamicFormComponent } from 'ae-dynamic-form';
 
 @Component({
@@ -15,21 +17,32 @@ export class FormComponent implements OnInit {
   @ViewChild(AeDynamicFormComponent) formElement: AeDynamicFormComponent;
 
   form = null;
-  constructor(private router: Router) { }
+  servie: EntityCollectionServiceBase<any>;
+  constructor(
+    private router: Router,
+    private projectDataService: ProjectDataService,
+    private issueDataSevice: IssueDataService,
+    private messageDataSevice: MessageDataService,
+    private userDataSevice: UserDataService,
+  ) { }
 
   ngOnInit(): void {
     switch (this.fromType) {
       case 'issue':
         this.form = issueForm;
+        this.servie = this.issueDataSevice;
         break;
       case 'project':
         this.form = projectForm;
+        this.servie = this.projectDataService;
         break;
       case 'user':
         this.form = userForm;
+        this.servie = this.userDataSevice;
         break;
       case 'message':
         this.form = messageForm;
+        this.servie = this.messageDataSevice;
         break;
       default:
         break;
@@ -39,6 +52,7 @@ export class FormComponent implements OnInit {
   submitted(formValue): void {
     this.event.emit(formValue);
     this.formElement.reset();
+    this.servie.upsert(formValue);
   }
 
   closeForm(): void {
